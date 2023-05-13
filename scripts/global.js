cf7cfc5101140fd2945e3a194d6ca011d8fd10e5
brainpowersindia <133396553+brainpowersindia@users.noreply.github.com>
2023-05-13 13:17:45 +0530

    Version 1.0

---
+++ scripts/global.js
@@ -0,0 +1,11 @@
+
+function navOpen() {
+    document.getElementById("navClose").style="display:block";
+    document.getElementById("navOpen").style="display:none";
+    document.getElementById("mobile").style=display="block";
+  }
+  function navClose() {
+      document.getElementById("navClose").style="display:none";
+      document.getElementById("navOpen").style="display:block";
+      document.getElementById("mobile").style="display:none";
+  }
